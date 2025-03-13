2b7d895dabd918cb9eed2fa666def75eba9dc06a
CHIRANJEEVI <developer.chiranjeevi@gmail.com>
2025-03-13 18:48:30 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Banner from "./components/Banner";
+
+
+
+function App(){
+  return(
+    <div className="">
+      {/* Components Master Container */}
+      <div className="">
+        <Banner />
+      </div>
+    </div>
+  );  
+  
+};
+
+
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+
+function Banner(){
+
+
+
+    return(
+        <div className="">
+            
+        </div>
+    );
+};
+
+
+
+
+export default Banner;
